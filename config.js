c7616cb722bdbd6adab1ab1d08964a28ce7e6bad
Juncheng Wu <wuj@aldenv104.allegheny.edu>
2019-02-22 16:33:24 -0500

    changed the config page

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  title: 'CreaToken', // Required
+  author: 'The Creative Token Market', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://creative-token-share.netlify.com/',
+    github: 'https://github.com/Lancasterwu',
+    twitter: 'https://twitter.com/YueshenWu',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/CreaToken',
+  siteUrl: 'https://creative-token-share.netlify.com/CreaToken',
 };
