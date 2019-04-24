070c51e7c376386b5164c4f5951efee838add6a9
Lancaster Wu <wuj@allegheny.edu>
2019-04-24 01:58:25 -0400

    change the structure of blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -56,6 +56,7 @@ class BlogPostTemplate extends React.Component {
             {userConfig.showShareButtons && (
               <Share url={url} title={post.frontmatter.title} />
             )}
+            <Comment title={post.frontmatter.title} />
           </Card>
 
           <PageNav>
